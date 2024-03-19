ab3a70949ce019669f56ab67ed22f186b204dddf
CoconutRemco <99071124@mydavinci.nl>
2024-03-19 13:38:48 +0100

    Log in pagina en register werkt nu nu bezig met spotify api

--- Jukebox/Mpapp/urls.py
+++ Jukebox/Mpapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.login_view, name='login'),  # Make login page as the main page
+    path('register/', views.register, name='register'),
+    path('jukebox/', views.jukebox, name='jukebox'),
 ]
