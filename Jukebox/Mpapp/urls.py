b7f291d6027e6051806d635e84fcb358a4b25162
CoconutRemco <99071124@mydavinci.nl>
2024-03-20 12:05:24 +0100

    Api communicatie werkt

--- Jukebox/Mpapp/urls.py
+++ Jukebox/Mpapp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.login_view, name='login'),  # Make login page as the main page
     path('register/', views.register, name='register'),
     path('jukebox/', views.jukebox, name='jukebox'),
+    path('spotify_auth/', views.spotify_auth, name='spotify_auth'),
+    path('spotify_callback/', views.spotify_callback, name='spotify_callback'),
+    path('spotify_request/', views.spotify_request, name='spotify_request'),
+    path('song/<str:song_id>/', views.song_detail, name='song_detail'),
 ]
