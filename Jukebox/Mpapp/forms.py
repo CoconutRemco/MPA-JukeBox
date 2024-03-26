32ce927a2a9c9271ccd7e93daed1d4c822ab8cb3
CoconutRemco <99071124@mydavinci.nl>
2024-03-26 09:26:28 +0100

    Alle data komt binnen in de database en word nu gebruikt om songs en bestaande playlists te tonen nu nog ervoor zorgen dat ik playlist kan aanpassen

---
+++ Jukebox/Mpapp/forms.py
@@ -0,0 +1,11 @@
+# Mpapp/forms.py
+
+from django import forms
+from .models import Playlist, Song
+
+class PlaylistForm(forms.ModelForm):
+    songs = forms.ModelMultipleChoiceField(queryset=Song.objects.all(), widget=forms.CheckboxSelectMultiple)
+
+    class Meta:
+        model = Playlist
+        fields = ['name', 'songs']
